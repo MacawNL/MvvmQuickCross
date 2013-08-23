8ac8c5f2034b8817d0cfae9cca1585839a65baf3
Vincent Hoogendoorn <vincent.hoogendoorn@macaw.nl>
2013-08-23 14:42:11 +0200

    Completed NuGet package install script

--- NuGet/Install.ps1
+++ NuGet/Install.ps1
@@ -1,13 +1,49 @@
 param($installPath, $toolsPath, $package, $project)
 
-# Change the namespace in the cs file from WinRT_RichTextBlock.Html2Xaml to the Common sub-namespace within the project default namespace
-$projectFolder = Split-Path -Path $project.FullName -Parent
-$csPath = Join-Path -Path $projectFolder -ChildPath "Common\RichTextBlockProperties.cs"
-$csContent = [System.IO.File]::ReadAllText($csPath)
-$defaultNamespace = $project.Properties.Item("DefaultNamespace").Value
-$csContent = $csContent.Replace("WinRT_RichTextBlock.Html2Xaml", "$defaultNamespace.Common")
-[System.IO.File]::WriteAllText($csPath, $csContent, [System.Text.Encoding]::UTF8)
+function ReplaceStringsInString([string]$text, [Hashtable]$replacements)
+{
+    foreach ($replacement in $replacements)
+    {
+        $text = $text.Replace($replacement.Name, $replacement.Value);
+    }
+    return $text
+}
 
-# Change the BuildAction of the xslt file to "Embedded Resource"
-$item = $project.ProjectItems.Item("Common").ProjectItems.Item("RichTextBlockHtml2Xaml.xslt")
-$item.Properties.Item("BuildAction").Value = 3;
+function ReplaceStringsInFile([string]$filePath, [Hashtable]$replacements)
+{
+    $content = [System.IO.File]::ReadAllText($filePath)
+    $content = ReplaceStringsInString -text $content -replacements $replacements
+    [System.IO.File]::WriteAllText($filePath, $content, [System.Text.Encoding]::UTF8)
+}
+
+function Install()
+{
+    # Get the application name from the solution file name
+    $solutionName = Split-Path ($project.DTE.Solution.FullName) -Leaf
+    $appName = $solutionName.Split('.')[0]
+    $defaultNamespace = $project.Properties.Item("DefaultNamespace").Value
+
+    $nameReplacements = @{
+        "_APPNAME_" = $appName
+    }
+
+    $contentReplacements = @{
+        "_APPNAME_" = $appName;
+        "MvvmQuickCross.Templates" = $defaultNamespace
+    }
+
+    $projectItems = (
+        $project.ProjectItems.Item("_APPNAME_Application.cs"),
+        $project.ProjectItems.Item("I_APPNAME_Navigator.cs"),
+        $project.ProjectItems.Item("ViewModels").ProjectItems.Item("_VIEWNAME_ViewModel.cs")
+    )
+
+    # Replace variables in project item names and content
+    foreach ($projectItem in $projectItems)
+    {
+        $projectItem.Name = ReplaceStringsInString -text $projectItem.Name -replacements $nameReplacements
+        ReplaceStringsInFile -filePath $projectItem.Properties.Item("FullPath").Value -replacements $contentReplacements
+    }
+}
+
+Install
