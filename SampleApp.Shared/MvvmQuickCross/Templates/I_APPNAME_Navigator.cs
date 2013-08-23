5cb6d5983d062997c0a64243ac6bb5db390bb642
Vincent Hoogendoorn <vincent.hoogendoorn@macaw.nl>
2013-08-23 12:03:42 +0200

    Added nuget template C# files

---
+++ SampleApp.Shared/MvvmQuickCross/Templates/I_APPNAME_Navigator.cs
@@ -0,0 +1,9 @@
+﻿namespace MvvmQuickCross.Templates
+{
+    public interface I_APPNAME_Navigator
+    {
+        /* TODO: For each view, add a method to navigate to that view with a signature like this:
+                void NavigateTo_VIEWNAME_View(object navigationContext);
+         */
+    }
+}
---
+++ SampleApp.Shared/MvvmQuickCross/Templates/ViewModels/_VIEWNAME_ViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MvvmQuickCross;
+
+namespace MvvmQuickCross.Templates.ViewModels
+{
+    public class _VIEWMODEL_ViewModel : ViewModelBase
+    {
+        public _VIEWMODEL_ViewModel()
+        {
+        }
+
+        #region Data-bindable properties and commands, generated with prop* and cmd* code snippets
+        #endregion
+    }
+}
+
+// VS Design-time data support
+namespace MvvmQuickCross.Templates.ViewModels.Design
+{
+    public class _VIEWMODEL_ViewModelDesign : _VIEWMODEL_ViewModel
+    {
+        public _VIEWMODEL_ViewModelDesign()
+        {
+            // TODO: Initialize the view model with hardcoded design-time data
+        }
+    }
+}
