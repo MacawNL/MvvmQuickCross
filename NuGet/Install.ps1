1aa948b156487abf6c1dcf009659c0330ad5d6e2
Vincent Hoogendoorn <vincent.hoogendoorn@macaw.nl>
2013-09-25 18:22:53 +0200

    Added platform and project type recognition to nuget install script

--- NuGet/Install.ps1
+++ NuGet/Install.ps1
@@ -4,7 +4,7 @@ function ReplaceStringsInString([string]$text, [Hashtable]$replacements)
 {
     foreach ($replacement in $replacements.GetEnumerator())
     {
-        $text = $text.Replace($replacement.Name, $replacement.Value);
+        $text = $text.Replace($replacement.Name, $replacement.Value)
     }
     $text
 }
@@ -22,7 +22,28 @@ function Install($project)
     $solutionName = Split-Path ($project.DTE.Solution.FullName) -Leaf
     $appName = $solutionName.Split('.')[0]
     $defaultNamespace = $project.Properties.Item("DefaultNamespace").Value
+    $projectFileContent = [System.IO.File]::ReadAllText($project.FullName)
 
+	# Determine the platform (android/ios/ws/wp) and project type (app / shared):
+    if ($projectFileContent -match '<\s*Reference\s+Include\s*=\s*"Mono.Android"') {
+        $platform = "android"
+        $isApplication = $projectFileContent -match '<\s*AndroidApplication\s*>\s*true\s*</\s*AndroidApplication\s*>'
+    } elseif ($projectFileContent -match '<\s*Reference\s+Include\s*=\s*"monotouch"') {
+        $platform = "ios"
+        $isApplication = $projectFileContent -match '<\s*OutputType\s*>\s*Exe\s*</\s*OutputType\s*>'
+    } elseif ($projectFileContent -match '<\s*ProjectTypeGuids\s*>[^<]*\{BC8A1FFA-BEE3-4634-8014-F334798102B3}[^<]*</\s*ProjectTypeGuids\s*>') {
+        # TODO: check if ws guid is the same in visual studio 2013
+        $platform = "ws"
+        $isApplication = $projectFileContent -match '<\s*OutputType\s*>\s*AppContainerExe\s*</\s*OutputType\s*>'
+    } elseif ($projectFileContent -match '<\s*ProjectTypeGuids\s*>[^<]*\{C089C8C0-30E0-4E22-80C0-CE093F111A43}[^<]*</\s*ProjectTypeGuids\s*>') {
+        # TODO: check if wp guid is the same in visual studio 2013
+        $platform = "wp"
+        $isApplication = $projectFileContent -match '<\s*SilverlightApplication\s*>\s*true\s*</\s*SilverlightApplication\s*>'
+    } else {
+        throw "Cannot determine target platform of project file " + $project.FullName
+    }
+
+    
     $nameReplacements = @{
         "_APPNAME_" = $appName
     }
@@ -32,17 +53,21 @@ function Install($project)
         "MvvmQuickCross.Templates" = $defaultNamespace
     }
 
-    $projectItems = (
-        $project.ProjectItems.Item("_APPNAME_Application.cs"),
-        $project.ProjectItems.Item("I_APPNAME_Navigator.cs"),
-        $project.ProjectItems.Item("ViewModels").ProjectItems.Item("_VIEWNAME_ViewModel.cs")
-    )
 
-    # Replace variables in project item names and content
-    foreach ($projectItem in $projectItems)
+    if (-not $isApplication) # Do the shared library file actions
     {
-        $projectItem.Name = ReplaceStringsInString -text $projectItem.Name -replacements $nameReplacements
-        ReplaceStringsInFile -filePath $projectItem.Properties.Item("FullPath").Value -replacements $contentReplacements
+        $projectItems = (
+            $project.ProjectItems.Item("_APPNAME_Application.cs"),
+            $project.ProjectItems.Item("I_APPNAME_Navigator.cs"),
+            $project.ProjectItems.Item("ViewModels").ProjectItems.Item("_VIEWNAME_ViewModel.cs")
+        )
+
+        # Replace variables in project item names and content
+        foreach ($projectItem in $projectItems)
+        {
+            $projectItem.Name = ReplaceStringsInString -text $projectItem.Name -replacements $nameReplacements
+            ReplaceStringsInFile -filePath $projectItem.Properties.Item("FullPath").Value -replacements $contentReplacements
+        }
     }
 }
 
