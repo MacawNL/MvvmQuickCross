488984c9950084e46d8b4cb029a9aa9f80870362
Vincent Hoogendoorn <vincent.hoogendoorn@macaw.nl>
2013-09-26 11:57:51 +0200

    Adding target framework support to nuget install

--- NuGet/Install.ps1
+++ NuGet/Install.ps1
@@ -22,27 +22,26 @@ function Install($project)
     $solutionName = Split-Path ($project.DTE.Solution.FullName) -Leaf
     $appName = $solutionName.Split('.')[0]
     $defaultNamespace = $project.Properties.Item("DefaultNamespace").Value
+    $targetFrameworkMoniker = $project.Properties.Item("TargetFrameworkMoniker").Value
+    # E.g. valid target framework monikers are:
+    # Windows Store:   .NETCore,Version=v4.5
+    # Windows Phone:   WindowsPhone,Version=v8.0
+    # Xamarin.Android: MonoAndroid,Version=v4.2
+    # Xamarin.iOS:     TODO MonoTouch,?
+    # Portable:        .NETPortable,Version=v4.5,Profile=Profile78
+
+    $targetFrameworkName = $targetFrameworkMoniker.Split(',')[0]
     $projectFileContent = [System.IO.File]::ReadAllText($project.FullName)
 
-	# Determine the platform (android/ios/ws/wp) and project type (app / shared):
-    if ($projectFileContent -match '<\s*Reference\s+Include\s*=\s*"Mono.Android"') {
-        $platform = "android"
-        $isApplication = $projectFileContent -match '<\s*AndroidApplication\s*>\s*true\s*</\s*AndroidApplication\s*>'
-    } elseif ($projectFileContent -match '<\s*Reference\s+Include\s*=\s*"monotouch"') {
-        $platform = "ios"
-        $isApplication = $projectFileContent -match '<\s*OutputType\s*>\s*Exe\s*</\s*OutputType\s*>'
-    } elseif ($projectFileContent -match '<\s*ProjectTypeGuids\s*>[^<]*\{BC8A1FFA-BEE3-4634-8014-F334798102B3}[^<]*</\s*ProjectTypeGuids\s*>') {
-        # TODO: check if ws guid is the same in visual studio 2013
-        $platform = "ws"
-        $isApplication = $projectFileContent -match '<\s*OutputType\s*>\s*AppContainerExe\s*</\s*OutputType\s*>'
-    } elseif ($projectFileContent -match '<\s*ProjectTypeGuids\s*>[^<]*\{C089C8C0-30E0-4E22-80C0-CE093F111A43}[^<]*</\s*ProjectTypeGuids\s*>') {
-        # TODO: check if wp guid is the same in visual studio 2013
-        $platform = "wp"
-        $isApplication = $projectFileContent -match '<\s*SilverlightApplication\s*>\s*true\s*</\s*SilverlightApplication\s*>'
-    } else {
-        throw "Cannot determine target platform of project file " + $project.FullName
+    switch ($targetFrameworkName)
+    {
+        'MonoAndroid'  { $platform = "android"; $define = '__ANDROID__'  ; $isApplication = $projectFileContent -match '<\s*AndroidApplication\s*>\s*true\s*</\s*AndroidApplication\s*>' }
+        'MonoTouch'    { $platform = "ios"    ; $define = '__IOS__'      ; $isApplication = $projectFileContent -match '<\s*OutputType\s*>\s*Exe\s*</\s*OutputType\s*>' }
+        '.NETCore'     { $platform = "ws"     ; $define = 'NETFX_CORE'   ; $isApplication = $projectFileContent -match '<\s*OutputType\s*>\s*AppContainerExe\s*</\s*OutputType\s*>' }
+        'WindowsPhone' { $platform = "wp"     ; $define = 'WINDOWS_PHONE'; $isApplication = $projectFileContent -match '<\s*SilverlightApplication\s*>\s*true\s*</\s*SilverlightApplication\s*>' }
+        '.NETPortable' { $platform = $null    ; $define = $null          ; $isApplication = $false }
+        default        { throw "Unsupported target framework: " + $targetFrameworkName }
     }
-
     
     $nameReplacements = @{
         "_APPNAME_" = $appName
@@ -53,8 +52,10 @@ function Install($project)
         "MvvmQuickCross.Templates" = $defaultNamespace
     }
 
-
-    if (-not $isApplication) # Do the shared library file actions
+    $installSharedCode = -not $isApplication
+    # TODO: ?check if the shared code is already present in another (referenced?) project in the solution, if not, install the shared code into the same project - to also support one-project solutions?
+    #       OR: if shared code not installed, fail and give message to install and reference first? nonblocking Dialog needed?
+    if ($installSharedCode) # Do the shared library file actions
     {
         $projectItems = (
             $project.ProjectItems.Item("_APPNAME_Application.cs"),
@@ -69,6 +70,14 @@ function Install($project)
             ReplaceStringsInFile -filePath $projectItem.Properties.Item("FullPath").Value -replacements $contentReplacements
         }
     }
+
+    # TODO: 
+    # If the folder for the target framework is not added automatically, copy and add it
+    # If the folders for the other frameworks are added automatically, remove them
+
+    # Add the #define for the target framework, if needed.
+    # In the readme, 
+
 }
 
 Install -project $project
