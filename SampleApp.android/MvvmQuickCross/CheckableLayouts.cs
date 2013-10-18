2ba783dd7da1650e08591c6df5db2beff6e1a5a6
Vincent Hoogendoorn <vincent.hoogendoorn@macaw.nl>
2013-10-18 14:57:20 +0200

    Android small cleanup

--- SampleApp.android/MvvmQuickCross/CheckableLayouts.cs
+++ SampleApp.android/MvvmQuickCross/CheckableLayouts.cs
@@ -10,6 +10,7 @@ namespace MvvmQuickCross
 {
     // Based on https://github.com/xamarin/monodroid-samples/blob/master/CustomChoiceList/CheckableLinearLayout.cs
     // For usage tip on propogating child state changes, see: http://chris.banes.me/2013/03/22/checkable-views/
+
     public class CheckableLinearLayout : LinearLayout, ICheckable
     {
         static readonly int[] CHECKED_STATE_SET = { Resource.Attribute.StateChecked };
@@ -30,14 +31,33 @@ namespace MvvmQuickCross
         public bool Checked
         {
             get { return _checked; }
-            set
-            {
-                if (_checked != value)
-                {
-                    _checked = value;
-                    RefreshDrawableState();
-                }
-            }
+            set { if (_checked != value) { _checked = value; RefreshDrawableState(); } }
+        }
+
+        public void Toggle() { Checked = !Checked; }
+    }
+
+    public class CheckableRelativeLayout : RelativeLayout, ICheckable
+    {
+        static readonly int[] CHECKED_STATE_SET = { Resource.Attribute.StateChecked };
+        private bool _checked = false;
+
+        public CheckableRelativeLayout(Context context) : base(context) { }
+        public CheckableRelativeLayout(Context context, IAttributeSet attrs) : base(context, attrs) { }
+        public CheckableRelativeLayout(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer) { }
+        public CheckableRelativeLayout(Context context, IAttributeSet attrs, int defStyle) : base(context, attrs, defStyle) { }
+
+        protected override int[] OnCreateDrawableState(int extraSpace)
+        {
+            int[] drawableState = base.OnCreateDrawableState(extraSpace + 1);
+            if (Checked) MergeDrawableStates(drawableState, CHECKED_STATE_SET);
+            return drawableState;
+        }
+
+        public bool Checked
+        {
+            get { return _checked; }
+            set { if (_checked != value) { _checked = value; RefreshDrawableState(); } }
         }
 
         public void Toggle() { Checked = !Checked; }
--- SampleApp.android/MvvmQuickCross/Templates/_VIEWNAME_ActivityView.cs
+++ SampleApp.android/MvvmQuickCross/Templates/_VIEWNAME_ActivityView.cs
@@ -1,5 +1,4 @@
 ﻿#if TEMPLATE // To add a new activity view class: in the Visual Studio Package Manager Console (menu View | Other Windows), use the New-View command (enter "Get-Help New-View" for details). Alternatively: copy this file, then in the copy remove the enclosing #if TEMPLATE ... #endif lines and replace _VIEWNAME_ and _APPNAME_ with the viewmodel resp application name.
-// To add a new activity view class: copy this file, then in the copy remove the enclosing #if TEMPLATE ... #endif lines, replace _VIEWNAME_ with the view name and implement any missing _APPNAME_Application members that are called from below code.
 using Android.App;
 using Android.OS;
 using Android.Content.PM;
